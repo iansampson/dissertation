b238839e22810037563b8d4df5e6b5da93cbf2c6
Ian Sampson <ian_sampson@brown.edu>
2019-08-12 19:22:55 -0700

    Change Gnome.sh to Gnome.command

--- Gnome.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Users/iansampson/Library/Developer/Xcode/DerivedData/gnome-fphmumzrvguyjnagdboiwlfggxsy/Build/Products/Debug/gnome mla Dissertation.pdf Metadata.md Frontmatter.html 0.\ Introduction.md 1.\ Perec.md 2.\ Calvino.md 3.\ Bök.md 4.\ Bergvall.md 5.\ Bervin.md -toc
